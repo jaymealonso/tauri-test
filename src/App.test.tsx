6b3cd4f778cbe92f63674020bd1a974c4508b599
Jayme <jayme.alonso@gmail.com>
2022-07-02 14:43:59 +0100

    release more

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn MORE react/i);
+  const linkElement = screen.getByText(/learn and RELEASE MORE react/i);
   expect(linkElement).toBeInTheDocument();
 });
